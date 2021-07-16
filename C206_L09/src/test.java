28ed366c050bba887f731bab78f2de1e9d51841c
20018859 <20018859@LAPTOP-37RAT7K5>
2021-07-16 14:20:13 +0800

    First

---
+++ C206_L09/src/test.java
@@ -0,0 +1,14 @@
+
+/**
+ * I declare that this code was written by me. I will not copy or allow others
+ * to copy my code. I understand that copying code is considered as plagiarism.
+ *
+ * 20018859, 16 Jul 2021 2:18:17 pm
+ */
+
+public class test {
+	public static void main(String[] args) {
+
+	}
+
+}
